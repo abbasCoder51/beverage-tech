9aeee033d566ab55f507bf1dc35e083ec41222fa
3acdigital <3acdigital@gmail.com>
2024-02-19 12:22:34 +0000

    - Added the punk api to the api.php file

---
+++ config/api.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Apis
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party apis.
+    |
+    */
+
+    'punk-api' => [
+        'endpoint' => env('PUNK_API_ENDPOINT', 'https://api.punkapi.com/v2/')
+    ]
+];
